eb2dd51c91282353e5e3103f8125431981707a01
Redmond Pajela <r.pajela.558384@umindanao.edu.ph>
2025-12-19 03:17:54 +0800

    Delete SHS_Student_Registration_System directory

--- SHS_Student_Registration_System/database/connect_database.py
+++
@@ -1,8 +0,0 @@
-# connect_database.py
-def get_connection_params():
-    return {
-        "host": "localhost",
-        "user": "root",
-        "password": "",
-        "database": "shs_registration_system"
-    }
